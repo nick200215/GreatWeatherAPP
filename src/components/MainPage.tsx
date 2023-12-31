c3ff560a7f4735cde3e6d71f03299098726a8272
nick200215 <96425571+nick200215@users.noreply.github.com>
2023-12-31 11:01:28 +0400

    bugs fix

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,3 +1,81 @@
+// import { useEffect } from "react";
+// import {
+//   getCurrentWeather,
+//   getLocation,
+//   getWeatherForecast,
+// } from "../services/API";
+// import MainInfo from "./mainInfo";
+// import { useAppDispatch } from "../types/hook";
+// import {
+//   //   selectWeather,
+//   setCurrentWeather,
+//   setLocation,
+//   setWeatherForecast,
+// } from "../redux/weatherSlice";
+// import HourlyForecast from "./HourlyForecast";
+// import DayForecast from "./DayForecast";
+// import OtherInfo from "./OtherInfo";
+// import LocationSearch from "./Search";
+
+// const MainPage = () => {
+//   const dispatch = useAppDispatch();
+
+//   // eslint-disable-next-line @typescript-eslint/no-explicit-any
+//   const handleSearch = async (searchResult: any) => {
+//     // Handle the search result as needed, for example, update the state with the result.
+//     console.log("Search Result:", searchResult);
+//   };
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const locationData = await getLocation();
+//         const currentWeatherData = await getCurrentWeather();
+//         const weatherForecast = await getWeatherForecast();
+
+//         dispatch(setLocation(locationData));
+//         dispatch(setCurrentWeather(currentWeatherData));
+//         dispatch(setWeatherForecast(weatherForecast));
+//       } catch (error) {
+//         console.error(error);
+//       }
+//     };
+
+//     fetchData();
+//   }, [dispatch]);
+
+//   return (
+//     <div className=" w-full flex justify-center my-8">
+//       <div className="w-11/12  ">
+//         <p className=" hidden">
+//           <LocationSearch onSearch={handleSearch} />
+//         </p>
+//         <div className=" flex gap-6 mt-8">
+//           <div className="w-3/4  flex flex-col gap-5">
+//             <div className="px-14">
+//               <MainInfo />
+//             </div>
+//             <div className="bg-weather-scondBG rounded-3xl">
+//               <HourlyForecast />
+//             </div>
+//             <div className="bg-weather-scondBG rounded-3xl">
+//               <OtherInfo />
+//             </div>
+//           </div>
+//           <div className=" w-1/4 font-light bg-weather-scondBG rounded-3xl p-4 ">
+//             <DayForecast />
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default MainPage;
+
+//!!!!!!!!!!!11
+
+// MainPage.js
 import { useEffect } from "react";
 import {
   getCurrentWeather,
@@ -7,7 +85,6 @@ import {
 import MainInfo from "./mainInfo";
 import { useAppDispatch } from "../types/hook";
 import {
-  //   selectWeather,
   setCurrentWeather,
   setLocation,
   setWeatherForecast,
@@ -15,12 +92,14 @@ import {
 import HourlyForecast from "./HourlyForecast";
 import DayForecast from "./DayForecast";
 import OtherInfo from "./OtherInfo";
-import LocationSearch from "./Search";
+// import LocationSearch from "./LocationSearch"; // Corrected import
+import LocationSearch from "./Search"; // Corrected import
 
 const MainPage = () => {
   const dispatch = useAppDispatch();
 
-  const handleSearch = async (searchResult) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handleSearch = async (searchResult: any) => {
     // Handle the search result as needed, for example, update the state with the result.
     console.log("Search Result:", searchResult);
   };
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,9 +1,88 @@
+// import { useState } from "react";
+// import axios from "axios";
+
+// const LocationSearch = () => {
+//   const [searchTerm, setSearchTerm] = useState("");
+//   const [searchResults, setSearchResults] = useState(null);
+
+//   const handleSearch = async () => {
+//     try {
+//       const response = await axios.get(
+//         "https://weatherapi-com.p.rapidapi.com/search.json",
+//         {
+//           params: { q: searchTerm },
+//           headers: {
+//             "X-RapidAPI-Key":
+//               "1fdaf4db93msh3aa938a71e6953ep18ec6djsnc68866367d53",
+//             "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
+//           },
+//         }
+//       );
+
+//       setSearchResults(response.data);
+//       console.log("Search Result:", response.data);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
+
+//   // eslint-disable-next-line @typescript-eslint/no-explicit-any
+//   const handleInputChange = (e: { target: { value: any } }) => {
+//     const value = e.target.value;
+//     setSearchTerm(value);
+
+//     // Trigger the search on each input change
+//     handleSearch();
+//   };
+
+//   return (
+//     <div className="flex items-center">
+//       <input
+//         type="text"
+//         placeholder="Enter location"
+//         value={searchTerm}
+//         onChange={handleInputChange}
+//         className="border p-2 mr-2 text-black"
+//       />
+//       {searchResults && (
+//         <div>
+//           <p>City Names:</p>
+//           <ul>
+//             {searchResults.map((result) => (
+//               <li key={result.id}>{result.name}</li>
+//             ))}
+//           </ul>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default LocationSearch;
+
+//!!!!!!!!!!!!!!!!
+
 import { useState } from "react";
 import axios from "axios";
 
-const LocationSearch = () => {
+// Define a type for the expected API response
+interface SearchResult {
+  id: string;
+  name: string;
+  // Add other properties as needed based on the actual API response
+}
+
+interface LocationSearchProps {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  onSearch: (searchResult: any) => void; // Adjust the type as needed
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars, no-empty-pattern
+const LocationSearch: React.FC<LocationSearchProps> = ({}) => {
   const [searchTerm, setSearchTerm] = useState("");
-  const [searchResults, setSearchResults] = useState(null);
+  const [searchResults, setSearchResults] = useState<SearchResult[] | null>(
+    null
+  );
 
   const handleSearch = async () => {
     try {
